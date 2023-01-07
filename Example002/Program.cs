4874c265fb76492847addb8e2896a0b1f3ab5fbb
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-07 21:41:53 +0300

    Д/З Урок 2

---
+++ Example002/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Consol e.WriteLine("Hello, World!");
+// int num1 = 13;
+// int num2 = 5;
+// if (num1 > 3 &&  num2 > 3)
+// {
+//     Console.Write("yes");
+// }
+// else
+// {
+//     Console.Write("no");
+// } 
+
+
+double num1 = 3;
+// int num2 = 5;
+
+   Console.WriteLine(Math.Sqrt(25));
