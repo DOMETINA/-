03db922ac165c57c3962d9c26412653d270d4d6d
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-09 20:03:21 +0300

    Д/З Урок 2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -39,30 +39,47 @@
 
 
 
-double num1 = 3;
-// int num2 = 5;
+// double num1 = 3;
+// // int num2 = 5;
 
-for(int i = 1; i < 7; i ++)
+// for (int i = 1; i < 7; i++)
+// {
+//     if (i == 3 || i == 4) continue;
+
+//     Console.WriteLine(i);
+// }
+
+// Console.WriteLine("OK");
+
+// Console.Write("Введите число1: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// double result1 = num / 100;
+
+// double result2 = num % 10;
+
+
+// Console.Write(result1);
+// Console.Write(result2);
+
+
+Console.Write("Введите число 1: ");
+double num1;
+Console.Write("Введите число 2: ");
+double num2;
+ if double result1 = (num1/num2);
 {
-   if (i == 3 || i == 4)continue;
-   
-   Console.WriteLine(i);
+ Console.WriteLine("кратно");
 }
+    
 
-   Console.WriteLine("OK");
-
-Console.Write("Введите число1: ");
-double num = Convert.ToInt32(Console.ReadLine());
-double result1 = num
-{
-    Console.Write("yes");
-}
-else  
+   double result = num1 / num2 % 2;
+     
+   Console.WriteLine("не кратно, остаток (result)");  
 
 
-{
-    Console.Write("no");
-} // Console.Write("Введите число1: ");
+
+
+// Console.Write("Введите число1: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Введите число2: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
