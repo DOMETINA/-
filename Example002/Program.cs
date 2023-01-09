b29ed9ca912a2de8c66a562a203a21d54ce4dc9b
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-09 15:42:51 +0300

    Д/З Урок 2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -12,7 +12,27 @@
 // } 
 
 
-double num1 = 3;
-// int num2 = 5;
 
-   Console.WriteLine(Math.Sqrt(25));
+// double num1 = 3;
+// // int num2 = 5;
+
+// for(int i = 1; i < 7; i ++)
+// {
+//    Console.WriteLine(i);
+// }
+
+//    Console.WriteLine("OK");
+
+
+
+
+   double num1 = 2;
+
+while ( num1 < 10)
+{
+   Console.WriteLine(num1);
+   num1 += 2;
+}
+
+   Console.WriteLine("OK");
+
