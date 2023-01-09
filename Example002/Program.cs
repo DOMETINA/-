63be64ea38bd7b533c239d6ad45a951aa6fe55f3
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-09 18:10:07 +0300

    Д/З Урок 2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -26,13 +26,51 @@
 
 
 
-   double num1 = 2;
+// double num1 = 2;
 
-while ( num1 < 10)
+// while (num1 < 10)
+// {
+//     Console.WriteLine(num1);
+//     num1 += 2;
+// }
+
+// Console.WriteLine("OK");
+
+
+
+
+double num1 = 3;
+// int num2 = 5;
+
+for(int i = 1; i < 7; i ++)
 {
-   Console.WriteLine(num1);
-   num1 += 2;
+   if (i == 3 || i == 4)continue;
+   
+   Console.WriteLine(i);
 }
 
    Console.WriteLine("OK");
 
+Console.Write("Введите число1: ");
+double num = Convert.ToInt32(Console.ReadLine());
+double result1 = num
+{
+    Console.Write("yes");
+}
+else  
+
+
+{
+    Console.Write("no");
+} // Console.Write("Введите число1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 * num2)
+// {
+//     Console.Write("yes");
+// }
+// else
+// {
+//     Console.Write("no");
+// } 
