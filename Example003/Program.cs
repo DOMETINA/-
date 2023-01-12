86d4c61389b9a0ad45be9cbe55de7522147de44f
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-12 21:09:09 +0300

    Д/З Урок 3

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Console.Write("Введите число 1: ");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+// if (num1 == num2 * num2)
+// {
+//    Console.WriteLine("кратно");
+// }
+// else 
+// {
+//       Console.Write("не кратно, остаток ");
+//     Console.Write(num1 / num2);
+// }
+
+// Задача 19// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Задача 21// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.5
+Console.Write("Введите число A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B: ");
+double B = Convert.ToDouble(Console.ReadLine());
+double A = (x1,y1,z1)
+double B = (x2,y2,z2)
+
+
+  Console.WriteLine
+
+
+
+
+
+// Задача 23// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
