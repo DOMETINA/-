938f279a4004e9f9ae3871843576314143238e7b
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-13 19:29:03 +0300

    Д/З Урок 3

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -14,23 +14,62 @@ Console.WriteLine("Hello, World!");
 //     Console.Write(num1 / num2);
 // }
 
-// Задача 19// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+
+
+// Console.Write("Введите число1: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число2: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0)
+// {
+//   Console.WriteLine("1 четверть");
+// }
+// else if (x < 0 && y > 0)
+// {
+//   Console.WriteLine("2 четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//   Console.WriteLine("3 четверть");
+// }
+
+// if (x > 0 && y < 0)
+// {
+//   Console.WriteLine("4 четверть");
+// }
+
+
+
+//int num = Convert.ToInt32(Console.ReadLine());
+//int [] nums = new int [5];
+//int num = nums[5];
+
+//int i = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < nums.Length; i++)
+// {
+
+//  Console.WriteLine(nums[i]);
+  // Console.WriteLine("ДА");
+
+   //else
+//Console.WriteLine("НЕТ");
+
+
+
+
 
 // Задача 21// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.5
-Console.Write("Введите число A: ");
-double A = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите число B: ");
-double B = Convert.ToDouble(Console.ReadLine());
-double A = (x1,y1,z1)
-double B = (x2,y2,z2)
+// Console.Write("Введите число A: ");
+// double A = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// double B = Convert.ToDouble(Console.ReadLine());
+// double A = (x1,y1,z1)
+// double B = (x2,y2,z2)
 
 
-  Console.WriteLine
+//   Console.WriteLine
 
 
 
