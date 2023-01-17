f4bf2b82271904b972272d1c42e1eba442f84913
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-17 21:52:24 +0300

    Д/З Урок 8

---
+++ Example008/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и 
+// последнюю строку массива.
+
+Console.Write("Введите число m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m,n];
+for (int i = 0; i < array.GetLength (0); i++)
+{
+   for (int j = 0; j < array.GetLength (1); j++)
+   {
+    array[i,j] = new Random().Next(0,10);
+    System.Console.WriteLine($"{array [m,n]}  ");
+   }
+System.Console.WriteLine();
+}
+
+
+
+
+
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
