a32833de0242d04f45ff067b131eaade42599d33
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-19 22:09:25 +0300

    Д/З Урок 9

---
+++ Example009/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(PrintNamber( N ));
+
+string PrintNamber( int N)
+{
+    if ( N  <= 1) return "1";
+    
+    
+       return PrintNamber(N - 1) + N;  
+    
+}
+//string PrintNamber(num)
+//System.Console.WriteLine(PrintNamber(num));
+
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
