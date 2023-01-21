25d8b258b4185550e0cc046206d90f1b835bd5ed
Maria <m.a.r.i.a.23.13@ya.ru>
2023-01-22 02:12:28 +0300

    Д/З Урок 6

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -8,40 +8,102 @@
 // [6 7 3 6] -> [6 3 7 6]
 
 // int[] array = new int[5];
-// int result = 0;
-// for (int i = 0; i < 5; i++)
+// for (int i = 0; i < array.Length; i++)
 // {
-//     array[i] = new Random().Next(-9, 10);
-//     Console.Write($"{ array[i]}   ");
+//     array[i] = new Random().Next(0, 10);
+//     Console.Write($"{array[i]}   ");
 // }
-// Console.WriteLine();
-
-
-// for (int i = 0; i < 5; i++)
+// System.Console.WriteLine();
+// int[] array1 = new int[5];
+// for (int i = 0; i < array1.Length; i++)
 // {
-//     if (i == 5)
-//   result = i;   
-//     System.Console.WriteLine(result);
-    // array[i] = i -1;
-    // Console.Write($"{ array[i]}   ");
-}
-
+//     array1[array.Length-1-i] = array[i];
+// }
+// for (int i = 0; i < array1.Length; i++)
+// {
+//     Console.Write($"{ array1[i]}   ");
+// }
 
 
 // Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать 
 //треугольник с сторонами такой длины. Теорема о неравенстве треугольника: каждая сторона треугольника 
 //меньше суммы двух других сторон. 
-
+// Console.Write("Введите длину стороны а: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите длину стороны b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите длину стороны c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// if (a < b + c && b < a + c && c < a+ b)
+// System.Console.WriteLine("Треугольник существует");
+// else
+// System.Console.WriteLine("Треугольник не существует");
 
 // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int n = num;
+// string result = " ";
 
+// while (n > 0)
+// {
+// result = n % 2 + result;
+// n = n/2;
+// }
+// System.Console.WriteLine(result);
+
+
+//ДОМАШНЕЕ ЗАДАНИЕ
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+// int[] array = new int[10];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-10, 10);
+//     Console.Write($"{array[i]}   ");
+// }
+// System.Console.WriteLine();
+// for (int i = 0; i < array.Length; i++)
+//     if (array[i] > 0)
+
+//     {
+//         int x = 1;
+        
+//         Console.Write($"{x ==1}   ");
+       
+//     }
+
+
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
 //уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// System.Console.Write("Введите значения b1 ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите значения k1 ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите значения b2 ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// System.Console.Write("Введите значения k2 ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// if (k1==k2 && b1==b2)
+// {
+//     System.Console.WriteLine("Линии совпадают");
+// }
+// else if (k1==k2 ||b1==b2)
+// {
+//     System.Console.WriteLine("Линии не пересекаются");
+// }
+// else if (k1!=k2 && b1!=b2)
+// {
+// double x = (b2-b1)/(k1-k2);
+// double y = k1*x +b1;
+//     System.Console.WriteLine($"Линии пересекаются в точке ({x},{y})" );
+// }
+
+
